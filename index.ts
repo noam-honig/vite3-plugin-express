401f454787a8d8cd1bd17f010dc5271889373b73
Noam Honig <noam.honig@gmail.com>
2022-11-09 13:27:49 +0200

    ok

---
+++ index.ts
@@ -0,0 +1,12 @@
+export default function (path: string) {
+  return {
+    name: "vite3-plugin-express",
+    configureServer: async (server: any) => {
+      server.middlewares.use(async (req: any, res: any, next: any) => {
+        process.env['VITE'] = "true";
+        const { app } = await server.ssrLoadModule(path);
+        app(req, res, next);
+      })
+    },
+  }
+}
