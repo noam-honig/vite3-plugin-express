a4d720ac624c7669630abf1bf37c71632be93457
Noam Honig <noam.honig@gmail.com>
2022-11-13 10:55:44 +0200

    Improve error handling

--- index.ts
+++ index.ts
@@ -3,10 +3,14 @@ export default function express(path: string) {
     name: "vite3-plugin-express",
     configureServer: async (server: any) => {
       server.middlewares.use(async (req: any, res: any, next: any) => {
-        process.env['VITE'] = "true";
-        const { app } = await server.ssrLoadModule(path);
-        app(req, res, next);
-      })
+        process.env["VITE"] = "true";
+        try {
+          const { app } = await server.ssrLoadModule(path);
+          app(req, res, next);
+        } catch (err) {
+          console.error(err);
+        }
+      });
     },
-  }
+  };
 }
