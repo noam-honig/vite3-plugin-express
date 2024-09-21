b14d81d39e8827f60daad73075ed708ad971bee3
Noam Honig <noam.honig@gmail.com>
2024-09-21 22:50:52 +0300

    update

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
+import { config } from "dotenv";
 export default function express(path: string) {
   return {
     name: "vite3-plugin-express",
     configureServer: async (server: any) => {
       process.on("unhandledRejection", (reason, promise) => {});
       server.middlewares.use(async (req: any, res: any, next: any) => {
+        config();
         process.env["VITE"] = "true";
         try {
           const { app } = await server.ssrLoadModule(path);
